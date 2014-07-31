8f396f1a427ba0fc75f50ceef0a33b0eba3ee116
anichalakova <anichalakova@users.noreply.github.com>
2014-08-01 00:39:58 +0300

    JS

---
+++ JavaScript_Basics/5_DOM&Events/01_likeUnlikeButton.js
@@ -0,0 +1,14 @@
+﻿function changeText(){ 
+	var theButton = document.getElementById('theButton');
+	var buttonText = theButton.innerHTML;
+	if (buttonText == 'Like') {
+		buttonText = 'Unlike';
+	} else { 
+		buttonText = 'Like';
+	}
+
+	theButton.innerHTML = buttonText;
+}
+
+var theButton = document.getElementById('theButton');
+theButton.addEventListener('click', changeText, false);
---
+++ JavaScript_Basics/5_DOM&Events/04_numbersOnlyField.js
@@ -0,0 +1,17 @@
+﻿
+
+function numbersOnly(){
+	var field = document.getElementById('numberField');
+	var inputValue = field.value;
+	if (isNaN(inputValue)) {
+		var field = document.getElementById('numberField');
+		field.style.backgroundColor = 'red';
+		field.disabled = true;
+		var correctValue = field.value;
+		correctValue = correctValue.slice(0, (correctValue.length - 1));
+		field.value = correctValue;	
+	}
+setTimeout(function (){	field.style.backgroundColor = 'transparent';
+		field.disabled = false; field.focus()}, 2000);
+}
+
