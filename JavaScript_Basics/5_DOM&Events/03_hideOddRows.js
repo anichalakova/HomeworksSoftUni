0bb40dd6e169846f61c971b66d6f7dc5f13756b2
anichalakova <anichalakova@users.noreply.github.com>
2014-08-01 23:55:37 +0300

    Updated JS 5_DOM hideOddRows

--- JavaScript_Basics/5_DOM&Events/03_hideOddRows.js
+++ JavaScript_Basics/5_DOM&Events/03_hideOddRows.js
@@ -1,9 +1,20 @@
 ﻿var hideButton = document.getElementById('btnHideOddRows');
 
-hideButton.onclick = function hideOddRows(){
-	
+hideButton.onclick = function hideOddRows()
+{
 	var rows = document.getElementsByTagName('tr');
-	for (var i = 0; i < rows.length; i++) {
-		rows[i].parentNode.removeChild(rows[i]);		
+	var firstVisible;
+	
+	for (var j = 0; j < rows.length; j++) {
+		if (rows[j].style.display !== 'none') {
+			firstVisible = j;
+			break;
+		}
+	}
+	
+	var step = (firstVisible + 1)*2;	
+	for (var i = firstVisible; i < rows.length; i+= step) {		
+		rows[i].style.display = 'none';		
 	}	
 }
+
